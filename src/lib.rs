e5b6533c069bece14504b877960f38d19fddffae
Daniël de Kok <me@danieldk.eu>
2021-01-07 10:09:35 +0100

    Move SerializedNumberer to serialize module

--- src/lib.rs
+++ src/lib.rs
@@ -5,48 +5,8 @@ use std::hash::Hash;
 
 use serde_derive::{Deserialize, Serialize};
 
-/// Serialized representation for `Numberer`.
-///
-/// This exists because we do not need to serialize both the
-/// `values` and `numbers` fields of `Numberer`, since they
-/// represent the same information.
-#[derive(Eq, PartialEq, Serialize, Deserialize)]
-struct SerializedNumberer<T> {
-    values: Vec<T>,
-    start_at: usize,
-}
-
-impl<T> From<Numberer<T>> for SerializedNumberer<T>
-where
-    T: Clone + Eq + Hash,
-{
-    fn from(numberer: Numberer<T>) -> Self {
-        SerializedNumberer {
-            values: numberer.values,
-            start_at: numberer.start_at,
-        }
-    }
-}
-
-impl<T> From<SerializedNumberer<T>> for Numberer<T>
-where
-    T: Clone + Eq + Hash,
-{
-    fn from(serialized_numberer: SerializedNumberer<T>) -> Self {
-        let numbers = serialized_numberer
-            .values
-            .iter()
-            .enumerate()
-            .map(|(idx, val)| (val.clone(), idx + serialized_numberer.start_at))
-            .collect();
-
-        Numberer {
-            numbers,
-            values: serialized_numberer.values,
-            start_at: serialized_numberer.start_at,
-        }
-    }
-}
+mod serialize;
+use serialize::SerializedNumberer;
 
 /// Numberer for categorical values, such as features or class labels.
 #[serde(from = "SerializedNumberer<T>", into = "SerializedNumberer<T>")]
